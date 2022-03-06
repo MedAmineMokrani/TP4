ab74ad9119c1fb802bfc389b6b9f559a3539e213
Amine <mohamedamine.mokrani@esen.tn>
2022-03-06 19:53:13 +0100

    final version

--- sycaruseFile.py
+++ sycaruseFile.py
@@ -1,18 +1,33 @@
-def sycaruse (a):
+class file:
+    
+    @staticmethod
+    def Read ():
+        fich = open("number.txt","r")
         
-        while (a != 1):
-            if ( a % 2 == 0 and a > 0):
-                a = a/2
-                print(a)
-            elif (a % 2 == 1 and a > 0):
-                a =a*3 + 1
-                print(a)
-            f = open("myfile.txt", "w")
-            f.write(str(a))
+        return int(fich.read())
+
+    @staticmethod
+    def sycaruse (a):
+        
+            while (a != 1):
+                if ( a % 2 == 0 and a > 0):
+                    a = a/2
+                    print(a)
+                elif (a % 2 == 1 and a > 0):
+                    a =a*3 + 1
+                    print(a)
+                f = open("myfile.txt", "w")
+                f.write(str(a))
        
-        return a
+            return a
+    @staticmethod    
+    def Write(a):
+        fich = open("resultat.txt","w")
+        fich.write(str(a))
+        fich.close 
       
 
-f = open("number.txt", "r")
-
-sycaruse(int(f.read()))
+classe = file()
+read = classe.Read()
+write = classe.sycaruse(read)
+classe.Write(classe.sycaruse(write))
